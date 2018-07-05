236542287175030c0738ff6d72f2ef96bee5cd60
= <=>
2018-07-04 21:32:45 -0500

    Clase API REST

--- Actividades/rails/appPostgres2/app/controllers/biblioteca_controller.rb
+++ Actividades/rails/appPostgres2/app/controllers/biblioteca_controller.rb
@@ -1,7 +1,8 @@
 class BibliotecaController < ApplicationController
+  protect_from_forgery unless: -> {request.format.json?}
   def index
     @libros = Libro.all
-  
+
   end
   
   def nuevo
@@ -12,4 +13,33 @@ class BibliotecaController < ApplicationController
     libro = Libro.new(libro_nuevo)
     libro.save()
   end 
+
+  def get_libros
+    libros = Libro.all
+    response = []                          
+    libros.each do |l|
+      response.push({
+        titulo: l.nombre,
+        genero: l.genero,
+        descripcion: l.descripcion,
+        autor: l.autor.nombre
+      })
+    end 
+    
+    render json:{
+      libros: response
+    }
+  end
+
+  def crear
+    b =  eval(request.body.read)
+    a = Autor.find(1)
+    libro = Libro.new(b)
+    libro.autor= a
+    libro.save
+    render json:{
+      status: libro.id
+    }
+  end
+
 end
--- Actividades/rails/appPostgres2/config/routes.rb
+++ Actividades/rails/appPostgres2/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'biblioteca/index'
-  get 'biblioteca/nuevo'
+  get  'biblioteca/index'
+  get  'biblioteca/nuevo'
   post 'biblioteca/guardar'
+  get  'biblioteca/get_libros'
+  get  'biblioteca/libro'
+  post 'biblioteca/crear'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
